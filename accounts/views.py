5674cc0d9dbdb09a4244a9123b0707a09c1ee3c6
hyejin beck <beckhye@gmail.com>
2023-08-28 16:52:10 +0900

    signup 폼

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def signup(request): 
+    if request.method == 'POST': 
+        pass 
+    else: 
+        # user가 id와 password입력받는 빈 폼 
+        form = CustomUserCreationForm()
+    
+    context = {
+        'form': form, 
+    }
+    return render(request,'signup.html', context)
